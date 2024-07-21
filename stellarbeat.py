3d8216f3cf9f69fd7a5acf6591f54d49be3d0afc
Giuliano Losa <giuliano@losa.fr>
2024-07-21 15:05:36 -0700

    persist data in stellarbeat.py

--- stellarbeat.py
+++ stellarbeat.py
@@ -2,39 +2,59 @@ import json
 import os
 from requests import get
 from platformdirs import user_cache_dir
+import fbas
 
-def _fetch_from_url() -> list:
+STELLARBEAT_URL = "https://api.stellarbeat.io/v1/node"
+validators = dict() # will be populated with data from stellarbeat at module initialization
+
+def _process_validators(json_data) -> list[dict]:
+        # raise if json_data is not a list:
+        if not isinstance(json_data, list):
+            raise ValueError("json_data must be a list")
+        result = dict()
+        for v in json_data:
+            match v:
+                case {'publicKey': _, 'quorumSet': _, 'isValidator': True}:
+                    if v['publicKey'] in result:
+                        raise ValueError(f"Duplicate validator found: {v['publicKey']}")
+                    result |= {v['publicKey'] : v}
+                case _:
+                    pass
+        return result
+
+def _fetch_from_url() -> dict:
     """
-    Get data from stellarbeat, filter out non-validator nodes, and return a list of dicts with two keys: 'publicKey' and 'quorumSet'.
+    Get data from stellarbeat and filter out non-validator nodes
     """
-    url = "https://api.stellarbeat.io/v1/node"
-    response = get(url, timeout=5)
+    response = get(STELLARBEAT_URL, timeout=5)
     if response.status_code == 200:
-        data = response.json()
-        return [{'publicKey': node['publicKey'], 'quorumSet': node['quorumSet']}
-            for node in data if node['isValidator']]
+        return _process_validators(response.json())
     else:
         response.raise_for_status()
 
-def get_validators(update=False) -> list:
+def get_validators(update=False) -> dict:
     cache_dir = user_cache_dir('python-fbas', 'SDF', ensure_exists=True)
     path = os.path.join(cache_dir, 'validators.json')
-
     if update:
         print(f"Updating data at {path}")
-        json_data = _fetch_from_url()
+        validators = _fetch_from_url()
         with open(path, 'w', encoding='utf-8') as f:
-            json.dump(json_data, f)
+            json.dump(validators, f)
     else:
         try:
             with open(path, 'r', encoding='utf-8') as f:
-                json_data = json.load(f)
+                validators = json.load(f)
         except FileNotFoundError:
-            json_data = _fetch_from_url()
+            validators = _fetch_from_url()
             with open(path, 'w', encoding='utf-8') as f:
-                json.dump(json_data, f)
-    return json_data
+                json.dump(validators, f)
+    return validators
 
-def get_validators_from_file(path) -> list:
-    with open(path, 'r', encoding='utf-8') as f:
-        return json.load(f)
+validators = get_validators()
+
+def org_of_qset(qs : fbas.QSet):
+    home_domains = {validators[v]['homeDomain'] for v in qs.validators}
+    if len(home_domains) == 1:
+        return home_domains.pop()
+    else:
+        return None
